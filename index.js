8bbaf3e912723598616000dd373cf43e632142ab
Peter Anderson <pete.anderson@gmail.com>
2019-09-23 20:38:55 +0200

    Removed test code

--- index.js
+++ index.js
@@ -15,21 +15,21 @@ const SpotifyStrategy = require("passport-spotify").Strategy;
 
 let userAccessToken = "";
 
-passport.use(
-    new SpotifyStrategy(
-        {
-            clientID: client_id,
-            clientSecret: client_secret,
-            callbackURL: "http://localhost:8080/callback"
-        },
-        function(accessToken, refreshToken, expires_in, profile, done) {
-            User.findOrCreate({ spotifyId: profile.id }, function(err, user) {
-                console.log(user);
-                return done(err, user);
-            });
-        }
-    )
-);
+// passport.use(
+//     new SpotifyStrategy(
+//         {
+//             clientID: client_id,
+//             clientSecret: client_secret,
+//             callbackURL: "http://localhost:8080/callback"
+//         },
+//         function(accessToken, refreshToken, expires_in, profile, done) {
+//             User.findOrCreate({ spotifyId: profile.id }, function(err, user) {
+//                 console.log(user);
+//                 return done(err, user);
+//             });
+//         }
+//     )
+// );
 
 passport.use(
     new SpotifyStrategy(
